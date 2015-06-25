c784f32b3b9489cebdc281c7265b334cdc5b8088
Clay Miller <clay@smockle.com>
2015-06-25 03:11:10 -0700

    Update list of Windows software I use.

--- install-windows.sh
+++ install-windows.sh
@@ -1,26 +1,23 @@
 #!/usr/bin/env sh
 
-# Install git # Includes Bash
-# Install io.js
-# Install git-credential-winstore
-# Install atom
-# Install Google Chome Canary
-# Install VisualStudio
-# Install hub (https://github.com/github/hub/releases)
+# Install hub
+# Install Go development environment (dependency)
+# Install git-credential-winstore (dependency)
 
 # Copy SSH keys
 start ~/.ssh
 
-# Install fasd and mktemp
+# Install fasd
 cd ~/Projects
 git clone clvv/fasd
-wget "http://downloads.sourceforge.net/project/mingw/MSYS/Extension/mktemp/mktemp-1.6-2/mktemp-1.6-2-msys-1.0.13-bin.tar.lzma?r=&ts=1413107452&use_mirror=hivelocity"
-# Extract mktemp to ~/Projects/fasd
+# Install mawk and mktemp (dependencies)
+# Send mktemp output to /dev/null
+
+# Install wget
 
 # Clone dotfiles
 cd ~/Projects
 git clone smockle/dotfiles
 
-# Update hosts file
-sudo mv /c/Windows/system32/drivers/etc/hosts ~/Projects/dotfiles/hosts.bak
-sudo cp ~/Projects/dotfiles/hosts /c/Windows/system32/drivers/etc/hosts
+# Install iojs
+# Install windosu
