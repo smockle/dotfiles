79686a5764409a7c8919ef12dc011ad0c28cc49a
Clay Miller <clay@smockle.com>
2018-01-25 16:43:40 -0800

    Add npm script and use npm from Homebrew

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+# Brew
+brew update
+brew install bash bash-completion coreutils git hub node@8 wget
+
+# Cask
+brew cask install bartender docker dropbox google-chrome-canary spectacle visual-studio-code-insiders
