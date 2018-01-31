56c0358255e2527cc47007ef5121d3282e4c7bb7
Clay Miller <clay@smockle.com>
2018-01-31 14:45:20 -0800

    Support personal and work casks in dotfiles

--- bin/brew.sh
+++ bin/brew.sh
@@ -1,9 +1,19 @@
 #!/usr/bin/env bash
 
-# Brew
-brew update
-brew install bash bash-completion coreutils git gnupg gpg-agent hub node@8 pinentry-mac wget
+PERSONAL=$(! grep -Fq "xamarin" "${HOME}/.npmrc"; echo $?)
+echo "${PERSONAL}"
 
-# Cask
+## Brew
+brew update
+brew install bash bash-completion coreutils git gnupg gpg-agent hub node@8 pinentry-mac watchman wget
+
+## Cask
 brew tap caskroom/versions
-brew cask install bartender docker dropbox google-chrome-canary spectacle visual-studio-code-insiders
+brew cask install bartender docker google-chrome-canary spectacle visual-studio-code-insiders
+if [ $PERSONAL -eq 0 ]; then
+  echo "PERSONAL"
+  brew cask install dropbox now steam
+else
+  echo "WORK"
+  brew cask install firefoxnightly iina microsoft-teams onedrive
+fi
