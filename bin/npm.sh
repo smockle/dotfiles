31610193f4a5e164ae7cb859e70c67de4c07fe57
Clay Miller <clay@smockle.com>
2018-02-06 22:56:34 -0800

    Set npm configuration

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,3 +1,15 @@
 #!/usr/bin/env bash
 
+# Install global npm modules
 npm i -g @smockle/contrast diff-so-fancy eslint flow-bin
+
+# When running `npm init`, suggest the MIT license (default: ISC license)
+npm config set init-license "MIT"
+
+# When running `npm init`, suggest me as the author
+npm config set init-author-email "clay@smockle.com"
+npm config set init-author-name "Clay Miller"
+npm config set init-author-url "https://smockle.com"
+
+# When running `npm version`, sign the git tag
+npm config set sign-git-tag true
