714276d0abc60808c60e00dd1b087077edea4904
Clay Miller <clay@smockle.com>
2018-03-04 22:45:00 -0800

    Update brew

--- bin/brew.sh
+++ bin/brew.sh
@@ -9,9 +9,9 @@ brew install bash bash-completion coreutils git gnupg gpg-agent hub node@8 pinen
 
 ## Cask
 brew tap caskroom/versions
-brew cask install bartender docker google-chrome spectacle visual-studio-code
+brew cask install bartender google-chrome spectacle visual-studio-code
 if [ $PERSONAL -eq 0 ]; then
-  brew cask install dropbox now steam
+  brew cask install dropbox steam
 else
-  brew cask install firefox iina microsoft-teams onedrive
+  brew cask install docker firefox iina microsoft-teams onedrive
 fi
