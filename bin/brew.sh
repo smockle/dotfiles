b2dffe80686878a6c2c012ec6e1bf559bef088df
Clay Miller <clay@smockle.com>
2018-03-06 14:15:17 -0800

    Install 'travis' on personal machines

--- bin/brew.sh
+++ bin/brew.sh
@@ -6,6 +6,9 @@ PERSONAL=$(! grep -Fq "xamarin" "${HOME}/.npmrc"; echo $?)
 ## Brew
 brew update
 brew install bash bash-completion coreutils git gnupg gpg-agent hub node@8 pinentry-mac watchman wget
+if [ $PERSONAL -eq 0 ]; then
+  brew install travis
+fi
 
 ## Cask
 brew tap caskroom/versions
