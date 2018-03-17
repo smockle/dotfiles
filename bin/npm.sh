6141fb49082045d60f764bb077c0151a26e68184
Clay Miller <clay@smockle.com>
2018-03-17 16:53:13 -0700

    Add 'www' to website address. Update year in LICENSE.

--- bin/npm.sh
+++ bin/npm.sh
@@ -9,7 +9,7 @@ npm config set init-license "MIT"
 # When running `npm init`, suggest me as the author
 npm config set init-author-email "clay@smockle.com"
 npm config set init-author-name "Clay Miller"
-npm config set init-author-url "https://smockle.com"
+npm config set init-author-url "https://www.smockle.com"
 
 # When running `npm version`, sign the git tag
 npm config set sign-git-tag true
