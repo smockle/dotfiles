01fd3ff9a268b6bda549cae3a1fedb3d6b40f2d9
Clay Miller <clay@smockle.com>
2018-06-13 14:25:47 -0700

    Add work global modules

--- bin/npm.sh
+++ bin/npm.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 
+# Determine whether to install personal or work modules
+PERSONAL=$(! grep -Fq "xamarin" "${HOME}/.npmrc"; echo $?)
+
 # Install global npm modules
 npm i -g @smockle/contrast diff-so-fancy
+if [ ! $PERSONAL -eq 0 ]; then
+  npm i -g @mobile-center/clusterficks
+fi
 
 # When running `npm init`, suggest the MIT license (default: ISC license)
 npm config set init-license "MIT"
