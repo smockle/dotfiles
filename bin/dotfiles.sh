6fd040fc9c47dd19ac6d5587b03ab99bf2e3afa1
Clay Miller <clay@smockle.com>
2018-08-10 12:40:37 -0700

    'Projects' to 'Developer'

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -2,25 +2,25 @@
 
 # Install packages via Homebrew
 echo "Installing packages via Homebrew…"
-bash "${HOME}/Projects/dotfiles/bin/brew.sh"
+bash "${HOME}/Developer/dotfiles/bin/brew.sh"
 printf "Installation complete.\n\n"
 
 # Install global npm modules
 echo "Installing modules via npm…"
-bash "${HOME}/Projects/dotfiles/bin/npm.sh"
+bash "${HOME}/Developer/dotfiles/bin/npm.sh"
 printf "Installation complete.\n\n"
 
 # Force create/replace symlinks
 echo "Creating symlinks…"
-bash "${HOME}/Projects/dotfiles/bin/link.sh"
+bash "${HOME}/Developer/dotfiles/bin/link.sh"
 printf "Symlinks created.\n\n"
 
 # Set custom macOS defaults
 echo "Setting macOS defaults…"
-bash "${HOME}/Projects/dotfiles/bin/defaults.sh"
+bash "${HOME}/Developer/dotfiles/bin/defaults.sh"
 printf "Defaults set.\n\n"
 
 # Install Visual Studio Code extensions
 echo "Installing Visual Studio Code extensions…"
-bash "${HOME}/Projects/dotfiles/bin/code.sh"
+bash "${HOME}/Developer/dotfiles/bin/code.sh"
 echo "Visual Studio Code extensions installed."
