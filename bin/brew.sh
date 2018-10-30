a950cb1b49498492e9eb119a13b95635e439514e
Clay Miller <clay@smockle.com>
2018-10-30 09:55:34 -0700

    Node.js v10

--- bin/brew.sh
+++ bin/brew.sh
@@ -5,7 +5,7 @@ PERSONAL=$(! grep -Fq "AppCenter" "${HOME}/.npmrc"; echo $?)
 
 ## Brew
 brew update
-brew install bash bash-completion git node@8 watchman
+brew install bash bash-completion git node@10 watchman
 if [ $PERSONAL -eq 0 ]; then
   brew install awscli
 else
@@ -14,9 +14,9 @@ fi
 
 ## Cask
 brew tap caskroom/versions
-brew cask install bartender docker google-chrome spectacle visual-studio-code
+brew cask install bartender docker google-chrome shifty spectacle visual-studio-code
 if [ $PERSONAL -eq 0 ]; then
   brew cask install dropbox
 else
-  brew cask install firefox microsoft-teams powershell sketch skype-for-business
+  brew cask install firefox microsoft-teams parallels@13 paw powershell sketch skype-for-business
 fi
