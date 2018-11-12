93e9cefd5eb4930bd85c75391ed2b5c851d3c578
Clay Miller <clay@smockle.com>
2018-11-12 11:58:56 -0800

    Use Code instead of Code Insiders

--- bin/brew.sh
+++ bin/brew.sh
@@ -14,7 +14,7 @@ fi
 
 ## Cask
 brew tap caskroom/versions
-brew cask install bartender docker google-chrome shifty spectacle visual-studio-code-insiders
+brew cask install bartender docker google-chrome shifty spectacle visual-studio-code
 if [ $PERSONAL -eq 0 ]; then
   brew cask install dropbox
 else
