ac3d4aa5c3464560e16a6813f9146785ff757edf
Clay Miller <clay@smockle.com>
2018-12-13 21:33:07 -0800

    Enable unattended-upgrades

--- bin/apt.sh
+++ bin/apt.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 # Install
-sudo apt-get install -y git make libavahi-compat-libdnssd-dev mosh vim
+sudo apt-get install -y git make libavahi-compat-libdnssd-dev mosh vim unattended-upgrades apt-listchanges
 
 # Node.js
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
