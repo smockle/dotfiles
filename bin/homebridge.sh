b7b037a8c687941415d21b93c92d0610e5bb6cf7
Clay Miller <clay@smockle.com>
2019-01-18 12:26:08 -0800

    Remove bluetoothd change

--- bin/homebridge.sh
+++ bin/homebridge.sh
@@ -11,13 +11,6 @@ if ! grep -qF -- "static domain_name_servers=1.1.1.1 1.0.0.1" /etc/dhcpcd.conf; 
     sudo echo "static domain_name_servers=1.1.1.1 1.0.0.1" >> /etc/dhcpcd.conf
 fi
 
-# Disable SIM Access Profile (fixes bluetooth systemd service)
-if [ -f /etc/systemd/system/dbus-org.bluez.service ]; then
-    sudo sed -i 's/^ExecStart=\/usr\/lib\/bluetooth\/bluetoothd/ExecStart=\/usr\/lib\/bluetooth\/bluetoothd --noplugin=sap/g' /etc/systemd/system/dbus-org.bluez.service
-    sudo systemctl daemon-reload
-    sudo systemctl restart bluetooth
-fi
-
 # Configure unattended upgrades
 if [ -f /etc/apt/apt.conf.d/50unattended-upgrades ]; then
     # Specify which packages can be updated
