3f6603688941e70760884401dc550491b9289413
Clay Miller <clay@smockle.com>
2019-02-01 23:22:16 -0800

    Install dig

--- bin/apt.sh
+++ bin/apt.sh
@@ -6,7 +6,7 @@ sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 
 # Install
-sudo apt-get install -y git make libavahi-compat-libdnssd-dev mosh vim unattended-upgrades apt-listchanges awscli
+sudo apt-get install -y git make libavahi-compat-libdnssd-dev mosh vim unattended-upgrades apt-listchanges awscli dnsutils
 
 # Node.js
 curl -sL https://deb.nodesource.com/setup_10.x | sudo bash -
