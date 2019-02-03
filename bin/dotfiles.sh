64303838c5d21a70b602142194ac66ce920bc878
Clay Miller <clay@smockle.com>
2019-02-03 13:56:13 -0800

    Split Homebridge/Pi-specific config

--- bin/dotfiles.sh
+++ bin/dotfiles.sh
@@ -1,18 +1,9 @@
 #!/usr/bin/env bash
 
-# Install packages via Apt
-if [[ $OSTYPE == "linux-gnu"* ]]; then
-  echo "Installing packages via Apt…"
-  bash "${HOME}/Developer/dotfiles/bin/apt.sh"
-  printf "Installation complete.\n\n"
-fi
-
 # Install packages via Homebrew
-if [[ $OSTYPE == "darwin"* ]]; then
-  echo "Installing packages via Homebrew…"
-  bash "${HOME}/Developer/dotfiles/bin/brew.sh"
-  printf "Installation complete.\n\n"
-fi
+echo "Installing packages via Homebrew…"
+bash "${HOME}/Developer/dotfiles/bin/brew.sh"
+printf "Installation complete.\n\n"
 
 # Install global npm modules
 echo "Installing modules via npm…"
@@ -25,22 +16,11 @@ bash "${HOME}/Developer/dotfiles/bin/link.sh"
 printf "Symlinks created.\n\n"
 
 # Set custom macOS defaults
-if [[ $OSTYPE == "darwin"* ]]; then
-  echo "Setting macOS defaults…"
-  bash "${HOME}/Developer/dotfiles/bin/defaults.sh"
-  printf "Defaults set.\n\n"
-fi
+echo "Setting macOS defaults…"
+bash "${HOME}/Developer/dotfiles/bin/defaults.sh"
+printf "Defaults set.\n\n"
 
 # Install Visual Studio Code extensions
-if [[ ! $(uname -a | grep raspberrypi) ]]; then
-  echo "Installing Visual Studio Code extensions…"
-  bash "${HOME}/Developer/dotfiles/bin/code.sh"
-  printf "Visual Studio Code extensions installed.\n\n"
-fi
-
-# Set up Homebridge
-if [[ $(uname -a | grep raspberrypi) ]]; then
-  echo "Setting up Homebridge…"
-  bash "${HOME}/Developer/dotfiles/bin/homebridge.sh"
-  echo "Homebridge setup complete."
-fi
+echo "Installing Visual Studio Code extensions…"
+bash "${HOME}/Developer/dotfiles/bin/code.sh"
+printf "Visual Studio Code extensions installed.\n\n"
