fb380e9f3f27e786ebc64afd250ac64c5ebec0bf
Clay Miller <clay@smockle.com>
2019-03-05 21:49:33 -0800

    Update MAS

--- dotfiles.sh
+++ dotfiles.sh
@@ -21,6 +21,7 @@ brew cask install bartender docker google-chrome shifty visual-studio-code \
 # mas
 mas install 409201541`#Pages` 409203825`#Numbers` 409183694`#Keynote` 497799835`#Xcode` \
   1333542190`#1Password7` 904280696`#Things3` 441258766`#Magnet` 425424353`#TheUnarchiver` 1320666476`#Wipr` \
+  ${PERSONAL:880001334}`#Reeder3` +${PERSONAL:+1225570693}`#Ulysses` ${PERSONAL:+924726344}`#Deliveries`
   ${WORK:+803453959}`#Slack` ${WORK:+1295203466}`#MicrosoftRemoteDesktop` ${WORK:+823766827}`#OneDrive` ${WORK:+462054704}`#MicrosoftWord`
 
 # npm
