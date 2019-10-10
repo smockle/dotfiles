7bcc2e85f65f612c0938a66ed8bce1d537e8bdd1
Clay Miller <clay@smockle.com>
2019-10-09 22:11:14 -0400

    Update installed packages and extensions

--- dotfiles.sh
+++ dotfiles.sh
@@ -6,13 +6,14 @@ DOTFILES_DIRECTORY=$(cd "${0%/*}" && pwd -P)
 # brew
 brew tap homebrew/cask-versions
 brew install clang-format diff-so-fancy git mas node@10 svgcleaner
-brew cask install docker figma google-chrome hazel visual-studio-code zoomus
+brew cask install docker figma google-chrome hazel sketch visual-studio-code zoomus
 
 # mas
 mas install 409201541`#Pages` 409203825`#Numbers` 409183694`#Keynote` \
   1333542190`#1Password7` 425424353`#TheUnarchiver` 1320666476`#Wipr` \
-  904280696`#Things3` 1449412482`#Reeder4` 924726344`#Deliveries` 1384080005`#Tweetbot` \
-  775737590`#iAWriter` 803453959`#Slack`
+  904280696`#Things3` 1482527526`#lire` 924726344`#Deliveries` \
+  1384080005`#Tweetbot` 775737590`#iAWriter` 803453959`#Slack` \
+  497799835`#Xcode`
 
 # npm
 npm config set init-license "MIT"
