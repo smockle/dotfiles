6f840ab59f9176823f35ddc40b124a701f9f9da0
Clay Miller <clay@smockle.com>
2020-07-09 17:52:01 -0400

    Update 'res.sh'

--- server/res.sh
+++ server/res.sh
@@ -17,8 +17,16 @@ client_ip=$(netstat -n | grep 5900 | tr -s ' ' | cut -d' ' -f5 | sed -En "s/^(19
 # Get the resolution of the client (e.g. "1280 800")
 client_res=$(ssh $client_ip 'export PATH=/usr/local/bin:$PATH; display_manager.py show main' | grep resolution | sed -En "s/^resolution\: +([0-9]+)x([0-9]+)$/\1 \2/p")
 
+client_width=$(echo $client_res | cut -d' ' -f1)
+client_height=$(echo $client_res | cut -d' ' -f2)
+
 # Set the host resolution
-display_manager.py res $client_res 60 only-hidpi main
+osascript <<EOF
+tell application "SwitchResX Daemon"
+	set res to modes of display 1 whose (width = $client_width and height = $client_height and definition = 2 and frequency = 60)
+	set current mode of display 1 to first item of res
+end tell
+EOF
 
 # Cache to avoid repeatedly setting host resolution
 touch "${HOME}/.res.tmp"
