74f04f83732f669c6d5dcbc5a81c0a36fb2781da
Clay Miller <clay@smockle.com>
2020-11-14 11:28:46 -0500

    Remove Home Assistant

--- dotfiles.sh
+++ dotfiles.sh
@@ -21,21 +21,18 @@ fi
 # Homebrew
 brew tap homebrew/cask
 brew tap homebrew/cask-versions
-[ -n "${SERVER}" ] && brew tap homebrew-ffmpeg/ffmpeg
 [ -n "${SERVER}" ] && brew tap homebrew/cask-drivers
 brew install bettertouchtool diff-so-fancy git hazel mas node@14 nova visual-studio-code \
-  ${SERVER:+adoptopenjdk8} ${SERVER:+awscli} ${SERVER:+libjpeg} ${SERVER:+switchresx} \
-  ${SERVER:+ubiquiti-unifi-controller} ${SERVER:+silicon-labs-vcp-driver} \
+  ${SERVER:+adoptopenjdk8} ${SERVER:+awscli} ${SERVER:+switchresx} ${SERVER:+ubiquiti-unifi-controller} \
   ${WORK:+adobe-creative-cloud} ${WORK:+docker} ${WORK:+encryptme} ${WORK:+figma} \
   ${WORK:+google-chrome} ${WORK:+paw} ${WORK:+sketch} ${WORK:+zoomus} \
   ${NOTSERVER:+shellcheck}
-[ -n "${SERVER}" ] && brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-fdk-aac
 brew link --overwrite --force node@14
 npm install --global --force npm@latest
 
 # App Store
-mas install 409201541`#Pages` 409203825`#Numbers` 409183694`#Keynote` \
-  1333542190`#1Password7` 425424353`#TheUnarchiver` 1320666476`#Wipr` \
+mas install 1333542190`#1Password7` 425424353`#TheUnarchiver` 1320666476`#Wipr` \
+  ${NOTSERVER:+409201541}`#Pages` ${NOTSERVER:+409203825}`#Numbers` ${NOTSERVER:+409183694}`#Keynote` \
   ${NOTSERVER:+1225570693}`#Ulysses` ${NOTSERVER:+1529448980}`#Reeder` \
   ${NOTSERVER:+290986013}`#Deliveries` ${NOTSERVER:+1477110326}`#Wikibuy` \
   ${WORK:+497799835}`#Xcode` ${WORK:+803453959}`#Slack` \
@@ -49,7 +46,10 @@ npm config set init-license "MIT"
 npm config set init-author-email "clay@smockle.com"
 npm config set init-author-name "Clay Miller"
 npm config set init-author-url "https://www.smockle.com"
+NPM_ROOT=$(npm root -g)
 [ -n "${SERVER}" ] && npm install --global homebridge homebridge-ring homebridge-mi-airpurifier homebridge-roomba-stv
+[ -n "${SERVER}" ] && ln -fs "${NPM_ROOT}/homebridge-ring/node_modules/ffmpeg-for-homebridge/ffmpeg" "/usr/local/bin/ffmpeg"
+unset NPM_ROOT
 
 # vi
 mkdir -p "${HOME}/.vim/backups"
@@ -70,6 +70,42 @@ ln -fs "${DOTFILES_DIRECTORY}/shell/.zshrc" "${HOME}/.zshrc"
 if [ ! -f "${HOME}/.ssh/config" ]; then
   mkdir -p "${HOME}/.ssh"
   cp "${DOTFILES_DIRECTORY}/ssh/config" "${HOME}/.ssh/config"
+fi
+
+# server
+if [ -n "${SERVER}" ]; then
+  declare -a LAUNCH_DAEMONS=(
+    "com.homebridge.ring.plist"
+    "com.homebridge.roomba.plist"
+    "com.homebridge.xiaomi.plist"
+    "com.smockle.ddns53.plist"
+    "com.smockle.wifi.plist"
+    "com.unifi.controller.plist"
+  )
+  declare -a LAUNCH_AGENTS=(
+    "com.smockle.res.plist"
+  )
+  for x in $LAUNCH_DAEMONS; do
+    if [ -f "${DOTFILES_DIRECTORY}/server/${x}" ]; then
+      echo "Loading ${x}"
+      sudo launchctl unload "/Library/LaunchDaemons/${x}" 2>/dev/null
+      sudo cp -f "${DOTFILES_DIRECTORY}/server/${x}" "/Library/LaunchDaemons"
+      sudo launchctl load "/Library/LaunchDaemons/${x}"
+      echo "Loaded ${x}"
+    fi
+  done
+  for x in $LAUNCH_AGENTS; do
+    if [ -f "${DOTFILES_DIRECTORY}/server/${x}" ]; then
+      echo "Loading ${x}"
+      launchctl unload "${HOME}/Library/LaunchAgents/${x}" 2>/dev/null
+      cp -f "${DOTFILES_DIRECTORY}/server/${x}" "${HOME}/Library/LaunchAgents"
+      launchctl load "${HOME}/Library/LaunchAgents/${x}"
+      echo "Loaded ${x}"
+    fi
+  done
+  unset x
+  unset LAUNCH_DAEMONS
+  unset LAUNCH_AGENTS
 fi
 
 # Visual Studio Code
